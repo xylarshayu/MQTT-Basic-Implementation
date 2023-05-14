c8e49ce8b880fd96442dba1d3fcb66278aa81daa
xylarshayu <xylar.w@gmail.com>
2023-05-14 21:21:37 +0530

    bugfix

--- mqtt_subscriber.py
+++ mqtt_subscriber.py
@@ -3,7 +3,7 @@ import paho.mqtt.client as mqtt
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
-    client.subscribe("iot/topic")
+    client.subscribe("iot/topic/#")
 
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
